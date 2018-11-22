a8cb9407f01c1f36d7385f982bf9a50ab54aae6b
DiegoAmancio <diego.pereira@ccc.ufcg.edu.br>
2018-11-21 23:21:31 -0300

    add testes de unidade com MockMvc

--- src/test/java/com/example/demo/WhemDocBackEndApplicationTests.java
+++ src/test/java/com/example/demo/WhemDocBackEndApplicationTests.java
@@ -2,15 +2,20 @@ package com.example.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import whenDoc.whenDOc.WhenDOcApplication;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = WhenDOcApplication.class)
+@TestPropertySource(locations="classpath:application.properties")
 public class WhemDocBackEndApplicationTests {
 
-	@Test
-	public void contextLoads() {
+	@Test	
+	public void contextLoads() {	
 	}
 
 }
--- src/test/java/whenDoc/whenDOc/WhenDOcApplicationTests.java
+++ src/test/java/whenDoc/whenDOc/WhenDOcApplicationTests.java
@@ -1,16 +1,60 @@
 package whenDoc.whenDOc;
 
+import javax.validation.constraints.NotEmpty;
+
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class WhenDOcApplicationTests {
+import com.example.demo.WhemDocBackEndApplicationTests;
 
+import net.minidev.json.JSONObject;
+import whenDoc.whenDOc.controller.MedicoController;
+
+
+public class WhenDOcApplicationTests extends WhemDocBackEndApplicationTests{
+	private MockMvc mockMvc;
+	
+	@Autowired
+	MedicoController medicoController;
+		
+	
+	@org.junit.Before
+	public void setUp() {
+		this.mockMvc = MockMvcBuilders.standaloneSetup(medicoController).build();
+	}
+	
+	@Test
+	public void testPUTSalarioMinimoController() throws Exception {
+		
+		this.mockMvc.perform(MockMvcRequestBuilders.post("/medico/cadastrar").contentType(org.springframework.http.MediaType.APPLICATION_JSON).
+				content(createUserInJson("ferreira99", "7653", "Taxista",  "3", "delicia", "123d", "213"))
+				
+				).andExpect(MockMvcResultMatchers.status().isCreated());
+		
+		
+	}
+	@Test
+	public void testPUTSalarioMinimoController2() throws Exception {
+		this.mockMvc.perform(MockMvcRequestBuilders.post("/medico/cadastrar").contentType(org.springframework.http.MediaType.APPLICATION_JSON).
+				content(createUserInJson("ferreira99", "7653", "Taxista",  "3", "delicia", "123d", "213"))
+				
+				).andExpect(MockMvcResultMatchers.status().isNotAcceptable());
+	}
 	@Test
 	public void contextLoads() {
-	}
-
+	}	
+	private static String createUserInJson (String nome, String crm, String especialidade,  String cpf,
+			String email,String senha, String telefone) {
+        return "{ \"nome\": \"" + nome + "\", " +
+                    "\"crm\":\"" + crm + "\"," +
+                    "\"especialidade\":\"" + especialidade + "\"," +
+                    "\"cpf\":\"" + cpf + "\"," +
+                    "\"email\":\"" + email + "\"," +
+                    "\"senha\":\"" + senha + "\"," +
+                    "\"telefone\":\"" + telefone + "\"}";
+    }
 }
